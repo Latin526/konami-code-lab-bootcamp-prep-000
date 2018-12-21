c52b9ad64fc6412142595e3be53c9d6ce46b607d
Yasmin Torres <yasmintorres90@hotmail.com>
2018-12-21 20:17:34 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,3 +1,4 @@
+
 const codes = [
   "ArrowUp",
   "ArrowUp",
@@ -12,5 +13,21 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+   let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
